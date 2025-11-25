22878e95712f824ec5716f01cbdb7e389ef02f8f
jaiswalrahul2427 <jaiswalrahul2427@gamil.com>
2025-11-25 19:01:35 +0530

    Merging

--- groupby.py
+++ groupby.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 data=pd.read_excel("employee_100_details.xlsx")
+print(data)
 gp=data.groupby("Job Title").agg({"Department":"count"})
 print(gp)
---
+++ merge.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data1={"Emp ID":["E01","E02","E03","E04","E05","E06"],
+      "Name":["Ram","Shyam","Rahul","Vishhal","Ravi","Aman"],
+      "Age":[34,35,25,80,60,55]}
+
+data2={"Emp ID":["E01","E02","E03","E04","E05","E06",],
+         "Salary":[45000,50000,60000,30000,45000,32000]  }
+
+df1=pd.DataFrame(data1)
+df2=pd.DataFrame(data2)
+
+print(df1)
+print(df2)
+1:09:54
